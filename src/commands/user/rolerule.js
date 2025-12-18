53676d4bb05dd35e4ccb6f2df6f8a078183efb15
Htoo <poelone259@gmail.com>
2025-12-18 15:11:20 +0630

    Fix rolerule export

---
+++ src/commands/user/rolerule.js
@@ -0,0 +1,14 @@
+module.exports = (bot) => {
+  bot.command("rolerule", (ctx) => {
+    ctx.reply(
+      "🏅 Role Rules\n\n" +
+      "0 pts - New Member\n" +
+      "500-999 pts - Bronze (Reward: 500)\n" +
+      "1000-1999 pts - Silver (Reward: 1000)\n" +
+      "2000-2999 pts - Gold (Reward: 2000)\n" +
+      "3000-4999 pts - Platinum (Reward: 4000)\n" +
+      "5000+ pts - Diamond (Reward: 5000)\n\n" +
+      "🎁 Monthly reward ကို လ၏ ၁ ရက်နေ့တွင် ပေးပါသည်"
+    );
+  });
+};
